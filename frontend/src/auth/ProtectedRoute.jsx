caf7af3f3a830b2f7578f098788e90606d49e275
Mohammed Hussain <mohussainan@gmail.com>
2026-07-18 23:24:44 +0530

    write protected route logic

--- frontend/src/auth/ProtectedRoute.jsx
+++ frontend/src/auth/ProtectedRoute.jsx
@@ -1,2 +1,14 @@
-// import { Navigate } from "react-router-dom";
-// import jwtDecode from "jwt-decoded";
+import { Navigate } from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
+
+export const ProtectedRoutes=({children})=>{
+const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/" />;
+  }
+
+  const decoded=jwtDecode(token);
+
+  return children;
+}
