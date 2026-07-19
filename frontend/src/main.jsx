ac001853c21f285d76f2bc4609e4847172468c58
Mohammed Hussain <mohussainan@gmail.com>
2026-07-19 18:58:12 +0530

    set authprovider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { MusicProvider } from "./context/MusicContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MusicProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </MusicProvider>
   </StrictMode>,
 );
