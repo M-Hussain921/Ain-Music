9964fb8642a260cc08d7df4636dcfc48725bda2f
Mohammed Hussain <mohussainan@gmail.com>
2026-07-19 13:26:20 +0530

    cors setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import { connectRedis } from "./config/redisClient.js";
 import { connectMongo } from "./config/mongoClient.js";
@@ -8,6 +9,7 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -16,7 +18,7 @@ app.use("/api/user",userRoutes);
 const startServer = async () => {
   await connectMongo();
   await connectRedis();
-  const port = process.env.PORT;
+  const port = process.env.PORT||5000;
 
   app.listen(port, () => console.log(`Server running on port ${port}`));
 };
