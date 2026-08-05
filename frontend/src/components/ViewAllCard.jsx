89800b893cfe345bc4a65097eb5d86b3337dec67
Mohammed Hussain <mohussainan@gmail.com>
2026-08-05 23:48:10 +0530

    change ui

--- frontend/src/components/ViewAllCard.jsx
+++ frontend/src/components/ViewAllCard.jsx
@@ -1,12 +1,10 @@
-import { FiPlus } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 export const ViewAllCard = ({ to }) => {
   return (
-    <Link to={to} className="flex flex-col items-center group">
-      <div className="w-full aspect-square rounded-xl border-2 border-dashed border-brand-light/40 flex items-center justify-center group-hover:border-brand-primary transition-colors gap-2">
-        <FiPlus className="text-3xl text-text-secondary group-hover:text-brand-primary" />
-        <p className="mt-3 text-sm font-semibold text-zinc-400 group-hover:text-white transition-colors">
+    <Link to={to} className="flex flex-col items-center group cursor-pointer">
+      <div className="w-full  rounded-xl border-2 border-dashed border-brand-light/40 flex items-center justify-center group-hover:border-brand-primary transition-colors gap-2">
+        <p className="px-2 py-1 text-sm font-semibold text-zinc-400 group-hover:text-brand-primary transition-colors">
           View All
         </p>
       </div>
