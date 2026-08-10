adde821bdba9484b1bf11543f2475c09c91e28d7
Mohammed Hussain <mohussainan@gmail.com>
2026-08-10 20:24:21 +0530

    update package.json and package-lock.json to include mongodb v7.5.0; add description for backend API add useNavigate hook in AlbumCard component for navigation to album details

--- frontend/src/components/AlbumCard.jsx
+++ frontend/src/components/AlbumCard.jsx
@@ -1,9 +1,11 @@
 import { AlbumPlayButton } from "./AlbumPlayButton";
 import { FavoriteButton } from "./FavoriteButton";
 import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const AlbumCard = ({ albums }) => {
-
+  const navigate=useNavigate();
+  
   return (
     <div
       onClick={() => navigate(`/album/${albums.id}`)}
